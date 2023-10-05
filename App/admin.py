3c72ed2454425143b76d98642194009dafb9e760
DanixMilek <danichandia77@gmail.com>
2023-10-05 18:42:19 -0300

    BD creada y Inicio Implementada

--- App/admin.py
+++ App/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Pintura,Curso
 
-# Register your models here.
+admin.site.register(Pintura)
+admin.site.register(Curso)
--- App/models.py
+++ App/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Pintura(models.Model):
+    id= models.BigAutoField(primary_key=True)
+    nombre=models.CharField(max_length=50)
+    precio=models.DecimalField(max_digits=9,decimal_places=2,default=0)
+    descripcion=models.CharField(max_length=300)
+    imagen=models.ImageField(upload_to='media/')
+
+    def __str__(self):
+        return str(self.nombre) + ": $" + str(self.precio)    
+class Curso(models.Model):
+    id= models.BigAutoField(primary_key=True)
+    nombre=models.CharField(max_length=50)
+    precio=models.DecimalField(max_digits=9,decimal_places=2,default=0)
+    descripcion=models.CharField(max_length=300)
+    imagen=models.ImageField(upload_to='media/',blank=True,default='curso_default.jpeg')
+    
+    def __str__(self):
+        return str(self.nombre) + ": $" + str(self.precio)    
