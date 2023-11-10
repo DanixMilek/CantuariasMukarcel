3fd471439a3acda602e273bb669ee62c61470baa
DanixMilek <danichandia77@gmail.com>
2023-11-10 13:29:22 -0300

    Primera versión de página web

---
+++ App/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Pintura,Curso
+
+admin.site.register(Pintura)
+admin.site.register(Curso)
---
+++ cantuariasmukarcel/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for cantuariasmukarcel project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cantuariasmukarcel.settings')
+
+application = get_asgi_application()
